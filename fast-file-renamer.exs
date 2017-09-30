53e9334c4915cabe066675884f271b2a4bef0b65
moonatique <schwartz.yoann@gmail.com>
2017-09-30 11:37:06 -0400

    Refactor of file to be more simple and catch extension

--- fast-file-renamer.exs
+++ fast-file-renamer.exs
@@ -1,34 +1,20 @@
-IO.puts "Hola, would you like to rename some .jpg file?"
+IO.puts "Hola, would you like continue to rename some files? If NOT tap ctrl + c"
 newFileName = IO.gets "What is your new file name?\n"
 newFileName = String.replace(newFileName, "\n", "")
 
-lastIndex = IO.gets "What is the last index you want start? just tap enter if 0\n"
-lastIndex = String.replace(lastIndex, "\n", "")
-lastIndex = cond do
-  lastIndex == "" ->
-    0
-  true ->
-    lastIndex |> Integer.parse |> elem(0)
-end
-
 File.ls!
   |> Enum.filter(&File.regular?/1)
-  |> Enum.filter(
-    fn fileName ->
-      idx = fileName
-        |> String.replace(~r/^.*-|\.jpg/, "")
-
-      idx = case Integer.parse(idx) do
-        {num, _} -> num
-        _ -> -1
-      end
-
-      idx == -1 || idx >= lastIndex
-    end
-  )
   |> Enum.with_index
   |> Enum.each(
     fn {fileName, idx} ->
-      File.rename "#{fileName}", "#{newFileName}-#{idx + lastIndex}.jpg"
+      ext = fileName
+        |> String.replace(~r/^.*\./, "")
+
+      File.rename "#{fileName}", "#{newFileName}-#{idx + 1}.#{ext}"
     end
   )
+
+IO.puts "\nFiles have been weel renamed.\n"
+File.ls!
+  |> Enum.filter(&File.regular?/1)
+  |> Enum.map(&IO.puts &1)
