fd23a6d7132693d410ab7dd8a33021252d5e3937
moonatique <schwartz.yoann@gmail.com>
2017-10-02 12:21:31 -0400

    change file name et create module for renamer function

---
+++ utils.exs
@@ -0,0 +1,26 @@
+defmodule Utils do
+  def fastFileRenamer(newFileName) do
+    File.ls!
+      |> Enum.filter(&File.regular?/1)
+      |> Enum.with_index
+      |> Enum.each(
+        fn {fileName, idx} ->
+          ext = fileName
+            |> String.replace(~r/^.*\./, "")
+
+          File.rename "#{fileName}", "#{newFileName}-#{idx + 1}.#{ext}"
+        end
+      )
+  end
+end
+
+IO.puts "Hola, would you like continue to rename some files? If NOT tap ctrl + c"
+newFileName = IO.gets "What is your new file name?\n"
+newFileName = String.replace(newFileName, "\n", "")
+
+Utils.fastFileRenamer(newFileName)
+
+IO.puts "\nFiles have been weel renamed.\n"
+File.ls!
+  |> Enum.filter(&File.regular?/1)
+  |> Enum.map(&IO.puts &1)
