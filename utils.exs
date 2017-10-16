68c1a2e0506113d498e506ef1a39dd7a6b2312bb
Moonatique <schwartz.yoann@gmail.com>
2017-10-16 11:15:33 -0400

    keep creation date order to rename files

--- utils.exs
+++ utils.exs
@@ -2,11 +2,30 @@ defmodule Utils do
   def fastFileRenamer(newFileName) do
     File.ls!
       |> Enum.filter(&File.regular?/1)
+      |> Enum.map(&extractDate/1)
+      |> Enum.sort
       |> Enum.with_index
       |> Enum.each(&rename_with_index &1, newFileName)
   end
 
-  defp rename_with_index({fileName, idx}, newFileName) do
+  defp extractDate(fileName) do
+    {:ok, %File.Stat{ctime: {date, time}}} = File.stat(fileName)
+
+    startDate =
+        [date, time]
+          |> Enum.map(&Tuple.to_list/1)
+          |> Enum.concat
+          |> Enum.map(&(if &1 < 10 do
+            "0#{&1}"
+          else
+            &1
+          end))
+          |> Enum.join
+
+    {fileName, startDate}
+  end
+
+  defp rename_with_index({{fileName, _}, idx}, newFileName) do
     ext = fileName
       |> String.replace(~r/^.*\./, "")
 
@@ -16,7 +35,7 @@ end
 
 IO.puts "Hola, would you like continue to rename some files? If NOT tap ctrl + c"
 newFileName = IO.gets "What is your new file name?\n"
-newFileName = String.replace(newFileName, "\n", "")
+newFileName = String.trim newFileName
 
 Utils.fastFileRenamer(newFileName)
 
